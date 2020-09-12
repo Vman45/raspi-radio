d882d0007c97ba5abeec7026e69e583308076ad5
ZimboPro <dejagerleslie@gmail.com>
2020-09-12 16:36:58 +0200

    Update setup.sh

--- modipy/setup.sh
+++ modipy/setup.sh
@@ -1,11 +1,12 @@
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
 sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/buster.list
 sudo apt-get update
-sudo apt-get install mopidy
+sudo apt-get install mopidy -y
+sudo apt install python3-pip -y
 
 # extensions
 echo "Installing Extensions"
-sudo apt-get install mopidy-spotify
-sudo apt-get install mopidy-mpd
+sudo apt-get install mopidy-spotify -y
+sudo apt-get install mopidy-mpd -y
 sudo python3 -m pip install Mopidy-YouTube
 sudo python3 -m pip install Mopidy-MusicBox-Webclient
