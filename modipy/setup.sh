d38fece83af5232774366c75dcf017bcff5fc38f
ZimboPro <dejagerleslie@gmail.com>
2020-09-15 19:23:49 +0200

    add root check

--- modipy/setup.sh
+++ modipy/setup.sh
@@ -1,3 +1,9 @@
+if [ $(id -u) -ne 0 ]
+  then
+  echo "Please run with root permissions"
+  exit 1
+fi
+
 wget -q -O - https://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
 sudo wget -q -O /etc/apt/sources.list.d/mopidy.list https://apt.mopidy.com/buster.list
 sudo apt-get update
